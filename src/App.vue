5ff1ab46de7218f8cb2d19aa4e5ba48f7706d1da
Henrique Almeida <henrique.almeida.moura@gmail.com>
2020-01-29 13:00:11 -0300

    Create component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
-<template>
-  <div>
-    <h1> {{ titulo }} </h1>
 
-    <ul>
-      <li v-for="foto of fotos" v-bind:key="foto.id">
-        <img :src='foto.url' :alt='foto.titulo'>
+<template>
+ 
+  <div class="corpo">
+    <h1 class="centralizado"> {{ titulo }} </h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" v-bind:key="foto.id">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src='foto.url' :alt='foto.titulo'>
+        </meu-painel>
+
         </li>
-      
     </ul>
      
   </div>
 </template>
 
 <script>
+
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
+  components:{
+    'meu-painel' : Painel
+  },
+
  data() {
    return{ 
      titulo:  'Pics',
@@ -34,5 +46,40 @@ export default {
 </script>
 
 <style>
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin:0 auto;
+  }
+  .centralizado{
+    text-align: center;
+  }
+
+  .lista-fotos{
+    list-style: none;
+  }
+  .lista-fotos .lista-fotos-item{
+    display: inline-block;
+  }
+    .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+  .imagem-responsiva{
+    width: 100%;
+  }
 
 </style>
