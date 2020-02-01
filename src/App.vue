b19dde1c551a18166f0ad47a47c812af5da3d17b
Henrique Almeida <henrique.almeida.moura@gmail.com>
2020-02-01 12:44:21 -0300

    Create verification in routes

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,22 @@
 <template>
  
   <div class="corpo">
-    <nav>
-      <ul>
-         <li v-for="route in routes" v-bind="route.path">
-            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
-          </li>
-      </ul>
-    </nav>
+    <meu-menu :rotas="routes"/>
 
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
 
 import {routes} from './routes';
+import Menu from './components/shared/menu/Menu.vue'
 export default {
+  components:{
+    'meu-menu': Menu
+  },
 
   data(){
     
@@ -37,4 +37,11 @@ export default {
     margin:0 auto;
   }
 
+  .pagina-enter, .pagina-leave-active {
+  opacity: 0
+}
+
+.pagina-enter-active, .pagina-leave-active {
+  transition: opacity .4s
+}
 </style>
--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,10 +1,10 @@
 <template>
-    <h2 cllas="centralizado">Cadastro</h2>    
+    <h2 class="centralizado">Cadastro</h2>    
 </template>
 <script>
 
 </script>
-<style>
+<style scoped>
     .centralizado{
         text-align: center
     }
