5e06e45217e7a571ee7600b74febd5be30f2daf2
Henrique Moura <henrique.almeida.moura@gmail.com>
2020-02-02 13:02:39 -0300

    naming routes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import Home from './components/home/Home.vue'
 
 export const routes = [
 
-    { path: '', component: Home, titulo: 'Home' },
-    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' }
-
+    { path: '', name:'home', component: Home, titulo: 'Home', menu:true  },
+    { path: '/cadastro',name:'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true, },
+    { path: '*', compoenent: Home, menu: false}
 ];
